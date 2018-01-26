c18e598f9123bbcd9f236b78ad2010eb967f36dc
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-26 15:13:33 +0100

    Options parser

---
+++ PGS.Azure.Storage.Queue/PGS.Azure.Storage.Queue/Configuration/StorageAccountOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace PGS.Azure.Storage.Queue.Configuration
+{
+    public class StorageAccountOptions
+    {
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public string QueueName { get; set; }
+    }
+}
--- PGS.Azure.Storage.Queue/PGS.Azure.Storage.Queue/Program.cs
+++ PGS.Azure.Storage.Queue/PGS.Azure.Storage.Queue/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using PGS.Azure.Storage.Queue.Configuration;
 
 namespace PGS.Azure.Storage.Queue
 {
@@ -6,7 +9,21 @@ namespace PGS.Azure.Storage.Queue
     {
         static void Main(string[] args)
         {
+            var options = ParseStorageAccountOptions();
             Console.WriteLine("Hello World!");
+        }
+
+        private static StorageAccountOptions ParseStorageAccountOptions()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var options = new StorageAccountOptions();
+            configuration.Bind("StorageAccount", options);
+            return options;
         }
     }
 }
