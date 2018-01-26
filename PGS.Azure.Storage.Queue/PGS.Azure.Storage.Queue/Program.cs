7e3314072fb3cc79d9b7d713d70b778d4b8b77df
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-26 14:13:37 +0100

    Empty project

---
+++ PGS.Azure.Storage.Queue/PGS.Azure.Storage.Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PGS.Azure.Storage.Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
